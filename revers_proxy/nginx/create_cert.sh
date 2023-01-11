ce992bb0c34bc8da53cff8b71845d212be6a2d55
Zany <nope>
2023-01-11 21:22:41 +0100

    bug fix

--- revers_proxy/nginx/create_cert.sh
+++ revers_proxy/nginx/create_cert.sh
@@ -3,7 +3,7 @@ set -e
 
 DOMAIN=$DOMAIN
 
-if [ ! -f /etc/letsencrypt/live/sysad.zany.ch/fullchain.pem ] || [ ! -f /etc/letsencrypt/live/sysad.zany.ch/privkey.pem ]; then
+if [ ! -f /etc/letsencrypt/live/$DOMAIN/fullchain.pem ] || [ ! -f /etc/letsencrypt/live/$DOMAIN/privkey.pem ]; then
     mkdir -p /etc/letsencrypt/dummy/$DOMAIN
     openssl req -batch -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/letsencrypt/dummy/$DOMAIN/privkey.pem  -out /etc/letsencrypt/dummy/$DOMAIN/fullchain.pem
 elif grep -wq "dummy" /etc/nginx/nginx.conf; then
