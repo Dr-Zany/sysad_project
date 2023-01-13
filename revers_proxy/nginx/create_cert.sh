3b920387c6306b49a2e2f9c06ab1eadde1875cb4
Zany <nope>
2023-01-13 15:48:35 +0100

    reworked env and added some doc

--- revers_proxy/nginx/create_cert.sh
+++ revers_proxy/nginx/create_cert.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -e
 
-DOMAIN=$DOMAIN
+NX_DOMAIN=$NX_DOMAIN
 
-if [ ! -f /etc/letsencrypt/live/$DOMAIN/fullchain.pem ] || [ ! -f /etc/letsencrypt/live/$DOMAIN/privkey.pem ]; then
-    mkdir -p /etc/letsencrypt/dummy/$DOMAIN
-    openssl req -batch -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/letsencrypt/dummy/$DOMAIN/privkey.pem  -out /etc/letsencrypt/dummy/$DOMAIN/fullchain.pem
+if [ ! -f /etc/letsencrypt/live/$NX_DOMAIN/fullchain.pem ] || [ ! -f /etc/letsencrypt/live/$NX_DOMAIN/privkey.pem ]; then
+    mkdir -p /etc/letsencrypt/dummy/$NX_DOMAIN
+    openssl req -batch -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/letsencrypt/dummy/$NX_DOMAIN/privkey.pem  -out /etc/letsencrypt/dummy/$NX_DOMAIN/fullchain.pem
 elif grep -wq "dummy" /etc/nginx/nginx.conf; then
     sed -i 's/dummy/live/' /etc/nginx/nginx.conf
 fi
